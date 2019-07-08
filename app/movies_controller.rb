46245bbb49b1bb3a21baad61425798abfbf365f1
Meg Galvez <meg@oakmaine.com>
2019-07-08 15:15:02 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_with_a_hash_of_attributes
     movie
 end
 
-def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1990})
+def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
   
   Movie.create do |m|
    
