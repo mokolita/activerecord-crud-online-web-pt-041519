13067aed201d6b38d3a2de160f07f9aba656ba4f
Meg Galvez <meg@oakmaine.com>
2019-07-08 15:53:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,7 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   Movie.create do |m|
     m.title = title 
     m.release_date = release_date
-    binding.pry
+    #binding.pry
   end
 end
 
