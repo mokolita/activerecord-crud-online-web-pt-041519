48a6ea190014155d7a6ebae5f6e5d20893a7de9d
Meg Galvez <meg@oakmaine.com>
2019-07-08 15:20:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,22 +30,15 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
+  
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = title 
+    m.release_date = release_date
+    m.save 
+    m
   end
 end
-
-#def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
-  
-  #Movie.create do |m|
-  #  m.title = title 
-  #  m.release_date = release_date
-   # m.save 
-    #m
-#  end
-#end
 
 def can_get_the_first_item_in_the_database
   __
