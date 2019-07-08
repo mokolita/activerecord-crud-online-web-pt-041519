b0cdea4fc8cecf0242fc0355cb9e16319f54a688
Meg Galvez <meg@oakmaine.com>
2019-07-08 15:42:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -82,7 +82,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
+  m = Movie.find_by(title: "Wat?")
+  m.title = "Wat, huh?"
   __
 end
 
