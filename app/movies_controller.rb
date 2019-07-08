b28685e8b7ad2a1291e0fb03c356319f754819e5
Meg Galvez <meg@oakmaine.com>
2019-07-08 15:13:27 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,8 +20,11 @@ def can_be_created_with_a_hash_of_attributes
       in_theaters: false
   }
   movie = Movie.new
-   movie.title = hash[:title]
-    movie.release_date = hash[:release_date]
+   movie.title = attributes[:title]
+    movie.release_date = attributes[:release_date]
+    movie.director = attributes[:director]
+    movie.lead = attributes[:lead]
+    movie.in_theaters = attributes[:in_theaters]
     movie.save 
 end
 
