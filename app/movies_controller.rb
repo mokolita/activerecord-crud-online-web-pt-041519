255b9a2c2b7680f445f79ab6b7498bc423ad45c9
Meg Galvez <meg@oakmaine.com>
2019-07-08 15:26:39 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -41,7 +41,7 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  
 end
 
 def can_get_the_last_item_in_the_database
